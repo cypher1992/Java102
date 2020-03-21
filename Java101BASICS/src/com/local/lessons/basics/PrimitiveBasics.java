7de8065ac769417ed0e7cb2e05951a6c11b24c51
Raymond Yassin <ryassin92@gmail.com>
2020-03-20 21:59:55 -0400

    Basics: Add Primitive Basics Class

---
+++ Java101BASICS/src/com/local/lessons/basics/PrimitiveBasics.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.local.lessons.basics;
+
+/**
+ * @author Zero
+ *
+ */
+public class PrimitiveBasics {
+
+	/**
+	 * Basics of Primitives 
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Integers Types: ");
+		byte var1 = 21;
+		System.out.println("Byte: " + var1);
+		short var2 = 1;
+		System.out.println("Short: " + var2);
+		int var3 = 123;
+		System.out.println("Int: " + var3);
+		long var4 = 231;
+		System.out.println("Long: " + var4);
+		
+		System.out.println("Characters Types ");
+		String var5 = "String isn\'t a primitive it is an object";
+		System.out.println("This is a String: "+ var5);
+		char var6 = 'C';
+		System.out.println("This is a char" + var6);
+		
+		System.out.println("Boolean Types");
+		boolean var7 = true;
+		System.out.println("This is a true Boolean type"+var7);
+		boolean var8 = false;
+		System.out.println("This is a false Boolean type"+var8);
+
+	}
+
+}
