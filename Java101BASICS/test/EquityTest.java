bc73aeb2978b8db25a2b0b45ccb7902d22ce9de0
Raymond Yassin <ryassin92@gmail.com>
2020-04-07 22:55:18 -0400

    Java Test Suite for Junit in Equity

---
+++ Java101BASICS/test/EquityTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EquityTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testInstrumentMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEquity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCompany() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCompany() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTicker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTicker() {
+		fail("Not yet implemented");
+	}
+
+}
