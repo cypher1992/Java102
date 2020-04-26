0363e86ded8e3a4f7659861370108649772e9903
Raymond Yassin <ryassin92@gmail.com>
2020-04-26 18:46:59 -0400

    Java101 portfolioException: add exception class for Portfolio

---
+++ Java101BASICS/src/com/local/lessson/financialclass/financialexception/PortfolioException.java
@@ -0,0 +1,8 @@
+package com.local.lessson.financialclass.financialexception;
+
+public class PortfolioException extends Exception {
+
+	public PortfolioException(String errMsg) {
+		super(errMsg);
+	}
+}
---
+++ Java101BASICS/test/com/local/financialtest/financialexceptiontest/PortfolioExceptionTest.java
@@ -0,0 +1,11 @@
+package com.local.financialtest.financialexceptiontest;
+
+import com.local.lessson.financialclass.financialexception.PortfolioException;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class PortfolioExceptionTest {
+
+
+}
