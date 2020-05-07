58c051820f1dc87af08f1e09e3599a36581fc9ec
Raymond Yassin <ryassin92@gmail.com>
2020-05-07 18:32:27 -0400

    Java101 NYSE: test ifInstanceof exchange or nyse

---
+++ Java101BASICS/test/com/local/financialtest/NYSETest.java
@@ -0,0 +1,22 @@
+package com.local.financialtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.local.lessson.financialclass.interfacexchange.NYSE;
+import com.local.lessson.financialclass.interfacexchange.Exchange;
+
+class NYSETest {
+
+	@Test
+	void testIsInstanceOfNYSE() {
+		NYSE nyse = NYSE.getInstance();
+		assertTrue(nyse instanceof NYSE);
+	}
+	
+	@Test
+	void testNYSEIsInstanceOfExchange() {
+		NYSE nyse = NYSE.getInstance();
+		assertTrue(nyse instanceof Exchange);
+	}
+
+}
