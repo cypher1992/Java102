f4529710ac06c05730f179519035b7eb54beb48e
Raymond Yassin <ryassin92@gmail.com>
2020-05-17 22:27:19 -0400

    Java101 Generics: add ListMethodsTest()

---
+++ Java101BASICS/test/com/local/lessons/genericstest/ListMethodsTest.java
@@ -0,0 +1,14 @@
+package com.local.lessons.genericstest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.local.lessons.generics.ListMethods;
+
+class ListMethodsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
