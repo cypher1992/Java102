3810986855ca09c6cc709c47839ddcd803ac9920
Raymond Yassin <ryassin92@gmail.com>
2020-05-18 21:32:47 -0400

    Java101 Generics: add test suite GenericMapTest

---
+++ Java101BASICS/test/com/local/lessons/genericstest/GenericMapTest.java
@@ -0,0 +1,14 @@
+package com.local.lessons.genericstest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.local.lessons.generics.GenericMapClass;
+
+class GenericMapTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
