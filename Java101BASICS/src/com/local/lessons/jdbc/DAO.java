8ee276c9d6bae70d07003b399a5fc999e8b332b1
Raymond Yassin <ryassin92@gmail.com>
2020-06-13 23:37:38 -0400

    Java101 JDBA: add delete(String column, String condition) and read(String column, String condition)

--- Java101BASICS/src/com/local/lessons/jdbc/DAO.java
+++ Java101BASICS/src/com/local/lessons/jdbc/DAO.java
@@ -3,5 +3,7 @@ package com.local.lessons.jdbc;
 public interface DAO {
 
 	public void add(String column ,String value);
-	public void update(String column ,String value,String condition); 
+	public void update(String column ,String value,String condition);
+	public void delete(String column ,String condition);
+	public void read(String column, String condition);
 }
