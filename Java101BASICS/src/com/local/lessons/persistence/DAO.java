bcfc453913f1849e7b28c8e9bcdb9ec48a155962
Raymond Yassin <ryassin92@gmail.com>
2020-06-15 22:22:35 -0400

    Java101 Persistence ListDAO: create class ListDAO<T>

---
+++ Java101BASICS/src/com/local/lessons/persistence/DAO.java
@@ -0,0 +1,9 @@
+package com.local.lessons.persistence;
+
+public interface DAO<T> {
+
+	public void add(T object);
+	public void update(T Object);
+	public void delete(T Object);
+	public void read(T Object);
+}
---
+++ Java101BASICS/src/com/local/lessons/persistence/listpersist/ListDao.java
@@ -0,0 +1,34 @@
+package com.local.lessons.persistence.listpersist;
+
+import java.util.List;
+
+import com.local.lessons.persistence.DAO;
+
+public class ListDao<T> implements DAO<T> {
+
+ 	
+	@Override
+	public void add(T object) {
+		
+		
+	}
+
+	@Override
+	public void update(T Object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(T Object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read(T Object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
