b6f820f1e70d1327aaa01b945b1b1df2b34d4f90
Raymond Yassin <ryassin92@gmail.com>
2020-06-27 22:47:49 -0400

    Java101 Persistence Stock: add test Stock Class

---
+++ Java101BASICS/test/com/local/persistence/stockstest/StockTest.java
@@ -0,0 +1,15 @@
+package com.local.persistence.stockstest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.local.lessons.persistence.stocks.*;
+
+import org.junit.jupiter.api.Test;
+
+class StockTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
