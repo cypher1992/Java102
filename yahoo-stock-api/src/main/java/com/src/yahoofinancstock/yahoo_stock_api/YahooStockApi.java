8a0a5747fef2306b9e0a2806362f38764dc57e06
Raymond Yassin <ryassin92@gmail.com>
2020-07-03 21:29:29 -0400

    Java101 Persistence Stock: Created Maven Project for Yahoo Stock Api

---
+++ yahoo-stock-api/src/main/java/com/src/yahoofinancstock/yahoo_stock_api/YahooStockApi.java
@@ -0,0 +1,13 @@
+package com.src.yahoofinancstock.yahoo_stock_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class YahooStockApi 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
