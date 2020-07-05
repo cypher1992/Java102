04e82c04c8fab7d84484774456430129227ccbf6
Raymond Yassin <ryassin92@gmail.com>
2020-07-05 16:10:42 -0400

    Java101 Persistence Stock: add variables for stats and ticker

---
+++ yahoo-stock-api/src/main/java/com/src/yahoofinancstock/yahoo_stock_api/StockData.java
@@ -0,0 +1,16 @@
+package com.src.yahoofinancstock.yahoo_stock_api;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+
+public class StockData 
+{
+	private Map<String,Object> stats = new TreeMap<String,Object>();
+	private String ticker; 
+	
+}
