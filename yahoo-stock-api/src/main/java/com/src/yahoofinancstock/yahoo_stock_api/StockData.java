3918e5a11dc8030aa2f15cbb3f3d46901ff28fe7
Raymond Yassin <ryassin92@gmail.com>
2020-07-06 20:03:17 -0400

    Java101 Persistence Stock: add pricetoearning to getStockStat()

--- yahoo-stock-api/src/main/java/com/src/yahoofinancstock/yahoo_stock_api/StockData.java
+++ yahoo-stock-api/src/main/java/com/src/yahoofinancstock/yahoo_stock_api/StockData.java
@@ -1,6 +1,7 @@
 package com.src.yahoofinancstock.yahoo_stock_api;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -19,9 +20,17 @@ public class StockData
 	
 	public Map getStockStats(){
 		
-		Long outstandingShares = YahooFinance.get(ticker).getStats().getSharesOutstanding()
+		Long outstandingShares = null;
+		BigDecimal pe = null;
+		try {
+			outstandingShares = YahooFinance.get(ticker).getStats().getSharesOutstanding();
+			pe = YahooFinance.get(ticker).getStats().getPe();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 		
-		stats.put("OUTSTANDINGSHARES", outstandingShares);		
+		stats.put("OUTSTANDINGSHARES", outstandingShares);
+		stats.put("PRICETOEARNING", pe);
 		return stats;
 		
 	}
