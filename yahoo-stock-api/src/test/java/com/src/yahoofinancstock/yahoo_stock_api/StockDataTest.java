94e0d60f77352c470b44480fae22d535982a910c
Raymond Yassin <ryassin92@gmail.com>
2020-07-08 21:10:01 -0400

    Java101 Persistence Stock: test getStockData(BX) return Map of BX stock stat

--- yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
+++ yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
@@ -1,9 +1,17 @@
 package com.src.yahoofinancstock.yahoo_stock_api;
 
 import junit.framework.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.TreeMap;
+
 import com.src.yahoofinancstock.yahoo_stock_api.StockData;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 /**
  * Unit test for simple App.
@@ -16,10 +24,33 @@ public class StockDataTest extends TestCase
      * @param testName name of the test case
      */
   
-	
 	public void testIsInstanceOfStockData() {
 		StockData sd = new StockData("BX");
 		Boolean isInstanceOfStockData = sd instanceof StockData ;
 		assertTrue(isInstanceOfStockData);
+	}
+	
+	public void testBXStockStatData() {
+		String ticker = "BX";
+		StockData sd = new StockData("BX");
+		Map<String,Object> actual = sd.getStockStats();
+		Map<String,Object> expected = new TreeMap<String,Object>();
+		try {
+			Long outstandingShares = YahooFinance.get(ticker).getStats().getSharesOutstanding();
+			BigDecimal pe = YahooFinance.get(ticker).getStats().getPe();
+			BigDecimal eps = YahooFinance.get(ticker).getStats().getEps();
+			BigDecimal ebitda = YahooFinance.get(ticker).getStats().getEBITDA();
+			expected.put("OUTSTANDINGSHARES", outstandingShares);
+			expected.put("PRICETOEARNING", pe);
+			expected.put("EARNINGPERSHARE", eps);
+			expected.put("EBITDA", ebitda);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(expected,actual);
+		
+		
 	}
 }
