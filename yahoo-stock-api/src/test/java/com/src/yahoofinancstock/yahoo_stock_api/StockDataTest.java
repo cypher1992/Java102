4a43494b9c96670821c84dc80de4b3375ba5a152
Raymond Yassin <ryassin92@gmail.com>
2020-07-17 21:58:46 -0400

    Java101 StockData: test bXLowLastIndexGetStockHistoricalTrades()

--- yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
+++ yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
@@ -254,4 +254,23 @@ public class StockDataTest extends TestCase
 		BigDecimal actual = (BigDecimal) actualMap.get("HIGH");
 		assertEquals(actual,expected);
 	}
+	
+	public void testBXLowLastIndexGetStockHistoricalTrades(){
+		String ticker = "BX";
+		StockData bx = new StockData(ticker);
+		List<HistoricalQuote> listOfTrades = null;
+		 try {
+			listOfTrades = YahooFinance.get(ticker).getHistory();
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}
+		SimpleDateFormat formatDate = new SimpleDateFormat("MMM-dd-yyyy");
+		Integer lastPosition = listOfTrades.size()-1;
+		String  simpleDate = formatDate.format(listOfTrades.get(lastPosition).getDate().getTime());
+		
+		BigDecimal expected = listOfTrades.get(lastPosition).getLow();
+		Map actualMap = (Map) bx.getStockHistoricalTrades().get(simpleDate);
+		BigDecimal actual = (BigDecimal) actualMap.get("LOW");
+		assertEquals(actual,expected);
+	}
 }
