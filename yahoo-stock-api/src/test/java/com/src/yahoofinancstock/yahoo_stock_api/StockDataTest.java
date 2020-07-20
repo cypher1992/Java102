c512b8c90eeea7a5964117f35a983da3d77b6dad
Raymond Yassin <ryassin92@gmail.com>
2020-07-20 19:11:56 -0400

    Java101 StockData: test BXOpenMiddleIndexGetStockHistoricalTrades()

--- yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
+++ yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
@@ -423,6 +423,29 @@ public class StockDataTest extends TestCase
 		assertEquals(actual,expected);
 	}
 	
+	public void testBXOpenMiddleIndexGetStockHistoricalTrades(){
+		String ticker = "BX";
+		StockData bx = new StockData(ticker);
+		List<HistoricalQuote> listOfTrades = null;
+		 try {
+			listOfTrades = YahooFinance.get(ticker).getHistory();
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}
+		SimpleDateFormat formatDate = new SimpleDateFormat("MMM-dd-yyyy");
+		Integer middlePosition = null;
+		if(listOfTrades.size()%2 == 0) {
+			middlePosition = listOfTrades.size()/2;
+		}else {
+			middlePosition = (listOfTrades.size()/2)+1;
+		}
+		String  simpleDate = formatDate.format(listOfTrades.get(middlePosition).getDate().getTime());
+		BigDecimal expected = listOfTrades.get(middlePosition).getOpen();
+		Map actualMap = (Map) bx.getStockHistoricalTrades().get(simpleDate);
+		BigDecimal actual = (BigDecimal) actualMap.get("OPEN");
+		assertEquals(actual,expected);
+	}
+	
 	
 	
 }
