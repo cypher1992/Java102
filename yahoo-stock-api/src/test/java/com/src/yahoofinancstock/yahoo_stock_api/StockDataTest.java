c95581014a8e8e39cc07fe0603f7c657cb576a27
Raymond Yassin <ryassin92@gmail.com>
2020-07-23 20:44:55 -0400

    JAVA101 StockData: test BXVolumeLastIndexGetStockHistoricalTradesInputs()

--- yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
+++ yahoo-stock-api/src/test/java/com/src/yahoofinancstock/yahoo_stock_api/StockDataTest.java
@@ -630,4 +630,23 @@ public class StockDataTest extends TestCase
 		assertEquals(actual,expected);
 	}
 	
+	public void testBXVolumeLastIndexGetStockHistoricalTradesInputs(){
+		String ticker = "BX";
+		StockData bx = new StockData(ticker);
+		Calendar from = new GregorianCalendar(2011,0,1);
+		Calendar to = new GregorianCalendar(2020,7,21);
+		List<HistoricalQuote> listOfTrades = null;
+		 try {
+			listOfTrades = YahooFinance.get(ticker).getHistory(from,to,Interval.DAILY);
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}
+		SimpleDateFormat formatDate = new SimpleDateFormat("MMM-dd-yyyy");
+		String  simpleDate = formatDate.format(listOfTrades.get(listOfTrades.size()-1).getDate().getTime());
+		
+		Long expected = listOfTrades.get(listOfTrades.size()-1).getVolume();
+		Map actualMap = (Map) bx.getStockHistoricalTrades(from,to,Interval.DAILY).get(simpleDate);
+		Long actual = (Long) actualMap.get("VOLUME");
+		assertEquals(actual,expected);
+	}
 }
