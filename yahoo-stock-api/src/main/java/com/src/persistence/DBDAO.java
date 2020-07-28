3c48bcbd3571a92fdbe92318e2ac88c81df9727a
Raymond Yassin <ryassin92@gmail.com>
2020-07-27 20:24:19 -0400

    JAVA101 DBDAO: add add(), remove(), query(), and update()

--- yahoo-stock-api/src/main/java/com/src/persistence/DBDAO.java
+++ yahoo-stock-api/src/main/java/com/src/persistence/DBDAO.java
@@ -1,6 +1,28 @@
 package com.src.persistence;
 
+import java.util.Map;
+
 public class DBDAO implements DAO<String, Object> {
+
+	public void add(Map<String, Object> stockPriceSeries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(Map<String, Object> stockPriceSeries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void query(String ticker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Map<String, Object> stockPriceSeries) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 	
