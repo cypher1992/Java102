cb28d23ced40b8f5fd57236a15ce5361caa7a54c
Raymond Yassin <ryassin92@gmail.com>
2020-08-06 20:35:14 -0400

    JAVA101 StringWrapper: add StringSingleQuoteMap(Map map):Map

--- yahoo-stock-api/src/main/java/com/srs/utility/StringWrapper.java
+++ yahoo-stock-api/src/main/java/com/srs/utility/StringWrapper.java
@@ -1,7 +1,22 @@
 package com.srs.utility;
-
+import java.util.Map;
+import java.util.Set;
 
 public class StringWrapper {
 
+	
+	public Map StringSingleQuoteMap(Map map) {
+		Set setKeys = map.keySet();
+		
+		for(Object key: setKeys) {
+			if(map.get(key) != null) {
+				map.put(key,"'" + map.get(key) +"'");
+			}
+		}
+		
+		return map;
+		
+	}
+	
 	
 }
