f4293d6f40446988be033776e1bd32983466030e
Raymond Yassin <ryassin92@gmail.com>
2020-08-06 20:48:53 -0400

    JAVA101 StringWrapper: test StringSingleQuoteMap(map) returns Map('50.00')

--- yahoo-stock-api/src/test/java/com/src/utilitytest/StringWrapperTest.java
+++ yahoo-stock-api/src/test/java/com/src/utilitytest/StringWrapperTest.java
@@ -1,15 +1,30 @@
 package com.src.utilitytest;
 
 import com.src.utility.StringWrapper;
+import java.util.Map;
+import java.util.TreeMap;
 
 import junit.framework.TestCase;
 
 public class StringWrapperTest extends TestCase {
 	
-	public void testIsInstanceOfStockData() {
+	public void testIsInstanceOfStringWrapper() {
 		StringWrapper sw = new StringWrapper();
 		Boolean isInstanceOfStringWrapper = sw instanceof StringWrapper ;
 		assertTrue(isInstanceOfStringWrapper);
+	}
+
+	
+	public void testStringSingleQuoteMapReturnsMapOfSingleQuotes() {
+		StringWrapper sw = new StringWrapper();
+		Map tm = new TreeMap<String,Object>();
+		double price = 50.00;
+		tm.put("PRICE", price);
+		Map actual = sw.StringSingleQuoteMap(tm);
+		Map expected = new TreeMap<String,Object>();
+		expected.put("PRICE", "'" + price + "'");
+		
+		assertEquals(actual,expected);
 	}
 
 }
