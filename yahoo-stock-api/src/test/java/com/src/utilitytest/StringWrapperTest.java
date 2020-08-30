480ec4fbe4949476ba2c5a072b453a39975f609a
Raymond Yassin <ryassin92@gmail.com>
2020-08-29 22:25:10 -0400

    JAVA101 StringWrapper: test three set values append string append Four Objects

--- yahoo-stock-api/src/test/java/com/src/utilitytest/StringWrapperTest.java
+++ yahoo-stock-api/src/test/java/com/src/utilitytest/StringWrapperTest.java
@@ -179,7 +179,7 @@ public class StringWrapperTest extends TestCase {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		String expected = "( BX, MUFA, WFC )";
+		String expected = "( BX, MUFA, WFC, INSERT_TS )";
 		assertEquals(actual,expected);
 	}
 	
