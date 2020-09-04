edb4b722359ec02674589d3bb66476431d761844
cypher1992 <ryassin92@gmail.com>
2020-09-03 22:12:52 -0400

    JAVA101 ADS LinkedList: add getters and setters for LinkedList()

--- Java101BASICS/src/com/local/ads/data_structures/LinkedList.java
+++ Java101BASICS/src/com/local/ads/data_structures/LinkedList.java
@@ -3,7 +3,15 @@ package com.local.ads.data_structures;
 public class LinkedList<T> {
 
 	private Node head;
-	
+		
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
 	class Node<T>{
 		
 		private T data;
