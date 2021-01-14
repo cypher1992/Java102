e84f4aaef1fc5e2c814119ac1c620088bc5cb951
cypher1992 <ryassin92@gmail.com>
2021-01-13 21:35:45 -0500

    JAVA:REVIEW:ITERATION: add class with variant iterationStructures

--- JavaRefresher/src/basics/IterationStructures.java
+++ JavaRefresher/src/basics/IterationStructures.java
@@ -1,5 +1,49 @@
 package basics;
 
 public class IterationStructures {
+	
+	private int start;
+	private int end;
+	static private int counter = 0; 
+	
+	public IterationStructures(int start,int end) {
+		this.start = start;
+		this.end = end;
+		
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	public static int getCounter() {
+		return counter;
+	}
+
+	public static void setCounter(int counter) {
+		IterationStructures.counter = counter;
+	}
+
+	public int forLoop(){
+		for(this.getStart();this.getCounter()<this.getEnd();this.counter++) {
+			this.setCounter(this.counter);
+			System.out.print(this.getCounter());
+		}
+		return this.getCounter();
+	}
+	
+	
 
 }
