d8be70f07b0251133eb57ba1c19dd46ba7556620
cypher1992 <ryassin92@gmail.com>
2021-01-24 21:11:13 -0500

    JAVA:COLLECTIONS:ARRAYLIST: test ArrayList3221RemoveIfEvenReturnList31

--- JavaRefresher/test/arraylisttest/ArrayListTest.java
+++ JavaRefresher/test/arraylisttest/ArrayListTest.java
@@ -265,12 +265,30 @@ class ArrayListTest {
 	void testArrayList221RemoveIfEvenReturnArrayList1() {
 		ArrayList<Integer> arraylist = new ArrayList<Integer>();
 		arraylist.add(2);
+		arraylist.add(2);
 		arraylist.add(1);
 		ArrayList<Integer> expected = new ArrayList<Integer>();
 		ClassArrayList arraycls = new ClassArrayList(arraylist);
 		Predicate<Integer> condition = index -> index%2==0;
 		arraycls.removeIf(condition);
 		ArrayList<Integer> actual = arraycls.getArrayList();
+		expected.add(1);
+		assertEquals(actual,expected);
+	}
+	
+	@Test
+	void testArrayList2321RemoveIfEvenReturnArrayList31() {
+		ArrayList<Integer> arraylist = new ArrayList<Integer>();
+		arraylist.add(2);
+		arraylist.add(3);
+		arraylist.add(2);
+		arraylist.add(1);
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+		ClassArrayList arraycls = new ClassArrayList(arraylist);
+		Predicate<Integer> condition = index -> index%2==0;
+		arraycls.removeIf(condition);
+		ArrayList<Integer> actual = arraycls.getArrayList();
+		expected.add(3);
 		expected.add(1);
 		assertEquals(actual,expected);
 	}
